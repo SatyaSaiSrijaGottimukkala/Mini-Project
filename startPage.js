14c0b8754b6450a61126e8f89cef47840994b71d
Maddela Vishnu Priya <37250846+vishnu2112@users.noreply.github.com>
2021-09-04 16:13:02 +0530

    Updated startPage.js
    Changed styles for bottom button

--- startPage.js
+++ startPage.js
@@ -1,50 +1,76 @@
 import React from 'react';
-import { StyleSheet,Button,StatusBar,Image,SafeAreaView,Text, TouchableHighlight,View, TextInput, TouchableOpacity } from 'react-native';
-//source={require('../images/001_villa.jpg')} 
+import { StyleSheet,Button,StatusBar,Image, SafeAreaView,Text, TouchableHighlight,View, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign'
+
 export default function StartPage() {
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title_logo}>LongStay Villa</Text>
-            <Image style = {styles.image} source= {require('../images/001_villa.jpg')} />
-            <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} >
-                <Text style={styles.loginText}>GET STARTED</Text>
-            </TouchableHighlight>
+            <View style={styles.box1}>
+                <Text style={styles.head}>LongStay Villa</Text>
+            </View>
+            <View style ={styles.box2}>
+                <Image style = {styles.img} source= {{uri:'https://training.pyther.com/yara/15-day/02-project-LongStay/004_villa.jpg'}} />
+            </View>
+            <View style={styles.box3}>
+                <TouchableHighlight style={[styles.buttonContainer,styles.button1]}>
+                    <View style={{flexDirection:'row'}}>
+                        <Text style={styles.loginText}>GET STARTED</Text>
+                        <Icon size = {30} style={styles.img1} name="arrowright"/>
+                    </View>
+                </TouchableHighlight>
+            </View>
         </SafeAreaView>
     )
-};
-
+}
 const styles = StyleSheet.create({
-    title_logo:{
+    container:{
         flex:1,
-        backgroundColor: '#ffffff',
-        fontSize:52,
-        fontWeight:'bold',
-        color:"#a52a2a",
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor:"#ffffff"
     },
-    image:{
-        flex:4,
-        flexDirection: 'row',
+    box1:{
+        flex:1.2,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    box2:{
+        flex:5, 
+    },
+    box3:{
+        flex:1.4,
+         alignItems:'flex-end'
+    },
+    head:{
+        color:"#b22222",
+        fontSize:53,
+        fontWeight:'bold'
+    },
+    img:{
+        flex: 1,
+        width: '100%',
+        height: '100%',
     },
     buttonContainer:{
-        flex:1,
-        flexDirection: 'row',
+       backgroundColor:"#b22222",
+       marginTop:35
+       
+    },
+    button1:{
+        width:230,
+        height:75,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 30,
-        backgroundColor:"#a52a2a",
+        borderTopLeftRadius:40,
+        borderBottomLeftRadius:40
     },
     loginText:{
-        color: 'white',
-        fontSize:30
+        color:"#ffffff",
+        fontSize:20,
+        marginRight:10,
+        marginTop:3
     },
-    container: {
-        flex: 1,
-        backgroundColor:"steelblue",
-    },
-    
+    img1:{
+        color:"#ffffff",
+        
+    }
 
-
-});
+})
