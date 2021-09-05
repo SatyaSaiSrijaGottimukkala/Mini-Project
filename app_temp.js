04c5ae0e359aea3057e51947a604637965ae07bd
rahulmacharya <66169141+rahulmacharya@users.noreply.github.com>
2021-09-05 22:28:57 +0530

    Update app_temp.js

--- app_temp.js
+++ app_temp.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-//import Login from './components/Login';
 import CustomerApp from './components/CustomerList';
 import Menu from './components/AppMenu';
 import AddCustomers from './components/AddCustomer';
